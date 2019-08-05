1ed2aac951aa75c6d7d9e52eb8cb9c741e11ccce
Darren Matthew <djmatthe@buffalo.edu>
2019-08-04 22:34:06 -0400

    did build

---
+++ build/precache-manifest.bc843bad296651739c96fd30fb5fa914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b7a2df205c686aec1dbd3ddc02c5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c64704f8685fa2d491",
+    "url": "/static/css/main.000d802b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e8c64704f8685fa2d491",
+    "url": "/static/js/main.73465d42.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
